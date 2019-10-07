cb7cc19b1c185b4156c9b0d43b670b8405a6d356
mateus-decassio <mateusdecassio.ferreira@gmail.com>
2019-10-07 11:55:11 -0300

    foi criado um script para renomear os arquivos de interesse

--- teste/execute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-	key="<Name>"
-
-	#grep "<Name>" Teste\ Mateus_49934-sg_HLA-A.tarr.xml
-	printf "$(grep "$key" Teste\ Mateus_49934-sg_HLA-A.tarr.xml | awk -F" " '{print $1}' | tr ">" " " | tr "<Name" " " | tr "/" " " | awk -F" " '{print $1}')\n"
