1350acfc82fa2c74463f348f39e24d935fbf1c17
mateus-decassio <mateusdecassio.ferreira@gmail.com>
2019-10-11 10:08:50 -0300

    filtro consertado para os casos especiais de arquivos

--- filter.py
+++ filter.py
@@ -3,9 +3,10 @@ import os
 import sys
 
 path = sys.argv[2]
-directories = ['undefined_phase', 'HLA-A', 'HLA-B', 'HLA-C',
+directories = ['undefined_phase', 'undefined_format', 'HLA-A', 'HLA-B', 'HLA-C',
                'HLA-DRB1', 'HLA-DQA1', 'HLA-DQB1', 'HLA-DPA1', 'HLA-DPB1']
 undefined_phase = path+"/undefined_phase"
+undefined_format = path+"/undefined_format"
 extension = ".xml"
 under = "_"
 
@@ -27,18 +28,48 @@ for i in archives:
     tree = ET.parse(file)
     root = tree.getroot()
 
-    s = root.xpath("./Samples/Sample/Name")
-    sample = s[0].text
+    if (root.tag == "ProjectXml"):
+        g = root.xpath("./Samples/Sample/Loci/Locus/NumberOfGenotypes")
+        numberofgenotypes = g[0].text
+        if (int(numberofgenotypes) > 0):
+            s = root.xpath("./Samples/Sample/Name")
+            sample = s[0].text
 
-    p = root.xpath("./Samples/Sample/Loci/Locus/PhasingRegions")
-    phasing = p[0].text
+            p = root.xpath("./Samples/Sample/Loci/Locus/PhasingRegions")
+            phasing = p[0].text
 
-    l = root.xpath("./Samples/Sample/Loci/Locus/Name")
-    locus = l[0].text
+            l = root.xpath("./Samples/Sample/Loci/Locus/Name")
+            locus = l[0].text
+
+            if (int(phasing) > 2):
+                os.rename(archive, undefined_phase+"/"+sample+under+locus+extension)
+            else:
+                os.rename(archive, path+"/"+locus+"/"+sample+extension)
+        else:
+            os.rename(archive, undefined_format+"/"+i+extension)
+
+
+    elif (root.tag == "Locus"):
+        g = root.xpath("./NumberOfGenotypes")
+        numberofgenotypes = g[0].text
+        if (int(numberofgenotypes) > 0):
+            s = root.xpath("./SampleName")
+            sample = s[0].text
+
+            p = root.xpath("./PhasingRegions")
+            phasing = p[0].text
+
+            l = root.xpath("./Name")
+            locus = l[0].text
+
+            if (int(phasing) > 2):
+                os.rename(archive, undefined_phase+"/"+sample+under+locus+extension)
+            else:
+                os.rename(archive, path+"/"+locus+"/"+sample+extension)
+        else:
+            os.rename(archive, undefined_format+"/"+i+extension)
+
+    else:
+        os.rename(archive, undefined_format+"/"+i)
 
     file.close()
-
-    if (int(phasing) > 1):
-        os.rename(path+"/"+i, undefined_phase+"/"+sample+under+locus+extension)
-    else:
-        os.rename(path+"/"+i, path+"/"+locus+"/"+sample+extension)
