57f4247ca683052dbde798ee9d34fe83fa1b626d
mateus-decassio <mateusdecassio.ferreira@gmail.com>
2019-11-18 10:54:14 -0300

    arquivo para instalar as dependências

--- dependencias.sh
+++ dependencias.sh
@@ -1,8 +1,9 @@
 #instalação da última versão do python
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.7
 sudo apt-get install python3
+sudo apt-get install python3.7
+
 
 
 #instalação da biblioteca lxml para a extração dos dados dos arquivos xml
